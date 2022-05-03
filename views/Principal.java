80a3d7023eb4e72c64cada0df39c5135e485bd91
WeskarMiranda <103000384+WeskarMiranda@users.noreply.github.com>
2022-05-03 13:11:54 -0300

    Update Principal.java

--- views/Principal.java
+++ views/Principal.java
@@ -9,37 +9,36 @@ public class Principal {
 		
 
 		do {
-			System.out.println("\n -- FOLHA DE PAGAMENTO -- \n");
+		    System.out.println("\n -- FOLHA DE PAGAMENTO -- \n");
 		    System.out.println("1 - Cadastrar funcionario");
 		    System.out.println("2 - Cadastrar pagamento");
 		    System.out.println("3 - Consultar pagamento");
 		    System.out.println("4 - Consultar todas as folhas de pagamento");
-            System.out.println("0 - Sair\n");
-            op = Console.readInt("Digite uma opção: ");
+            	    System.out.println("0 - Sair\n");
+                    op = Console.readInt("Digite uma opção: ");
 
 			switch (op) {
 				case 1: 
-                	CadastrarFuncionario cadastrarFuncionario = new CadastrarFuncionario();
-                	cadastrarFuncionario.renderizar();  
-            	break;
+                	        	CadastrarFuncionario cadastrarFuncionario = new CadastrarFuncionario();
+                			cadastrarFuncionario.renderizar();  
+            			break;
 				case 2:
-                	CadastrarPagamento cadastrarPagamento = new CadastrarPagamento();
-                	cadastrarPagamento.renderizar();
-            	break;
+                			CadastrarPagamento cadastrarPagamento = new CadastrarPagamento();
+                			cadastrarPagamento.renderizar();
+            			break;
 				case 3:
-                	CadastrarConsultarPagamento cadastrarConsultarPagamento = new CadastrarConsultarPagamento();
-                	cadastrarConsultarPagamento.renderizar();
-            	break;
-				case 4: 
-            	break;
-            	case 0: 
+                			CadastrarConsultarPagamento cadastrarConsultarPagamento = new CadastrarConsultarPagamento();
+                			cadastrarConsultarPagamento.renderizar();
+            			break;
+				
+            			case 0: 
 					System.out.println("Programa encerrado");
-            	break;
+            			break;
 				default:
 					System.out.println("Opcão invalida...");
 			}
-		} while (op!=0);
+	      	} while (op!=0);
 
-    }
+    	}
 	
 }
